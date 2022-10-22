1e0e51fb63f5453910295ebe515847d6d576158c
MeiiYuki <maituyettrinh2609@gmail.com>
2022-10-22 17:35:14 +0700

    Class

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculation
+    {
+        double num1, num2;
+
+    }
+    public Calculation(double num1, double num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+
+    }
+}
